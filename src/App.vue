14d4c4d93d761f9e926f0bc0282e23c9733bb6eb
anna.elinevich <anna.elinevich@lacit.net>
2021-02-07 22:55:25 +0300

    save

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+<MainPage></MainPage>
+<Footer/>
+  </div>
+</template>
+
+<script>
+import MainPage from './components/MainPage.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainPage,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/style/index.scss';
+
+
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+        <footer class="footer">
+<div class="logo-block">
+    <img :src="logo" alt="" class="logo">
+</div>
+        <div class="social-block">
+            <div class="social-link">
+
+<SvgSprite/>
+        <svg v-for="(icon, index) in icons" 
+        class="social-link__icon icon" 
+        :key="`${icon}_${index}`">
+        <use :xlink:href="`${icon}`">{{icon}}</use>
+    </svg>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+import SvgSprite from './SvgSprite'
+export default {
+    name: 'Footer',
+    components:
+    { SvgSprite : SvgSprite},
+    data: () => ({
+        isActive: false,
+        activeIndex: null,
+        logo: require('../assets/img/mainlogo.png'),
+        show: true,
+        icons: ['#linkedin', '#facebook', '#instagram', '#behance', '#telegram'],
+    }),
+    methods: {
+    }
+}
+</script>
+
